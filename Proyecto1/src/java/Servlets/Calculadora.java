4fb3cca4b24452509533ea2afba1fd2d785ae514
aula134 <aula134@UMANIZALES.LOCAL>
2017-03-30 21:15:54 -0500

    Primer Proyecto con JavaEE

---
+++ Proyecto1/src/java/Servlets/Calculadora.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aula134
+ */
+public class Calculadora extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter out = response.getWriter();
+        
+        int num1, num2 = 0;
+        String opr;
+        double result = 0;
+        
+        num1 = Integer.parseInt(request.getParameter("num1"));
+        num2 = Integer.parseInt(request.getParameter("num2"));
+        opr = request.getParameter("oper");
+        
+        if (opr.equals("sum")) {
+            result = num1+num2;
+        }else if (opr.equals("res")) {
+            result = num1-num2;
+        }else if (opr.equals("mul")) {
+            result = num1*num2;
+        }else if (opr.equals("div")) {
+            if (num2 == 0) {
+                JOptionPane.showMessageDialog(null, "No se puede realizar la división: Valor en 0");
+            }
+            else {
+                result = num1/num2;
+            }
+        }
+        out.print("El resultado es: "+result);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
